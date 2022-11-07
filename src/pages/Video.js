af30d2837203fdc79564ec04d1935919172da3ae
SUMON <m.sumon@outlook.com>
2022-11-07 19:38:09 +0600

    channel details added

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -6,6 +6,7 @@ import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
 import AddTaskOutlinedIcon from "@mui/icons-material/AddTaskOutlined";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import channelImage from "../img/videoimage.png"
 
 const Container = styled.div`
 display:flex;
@@ -51,6 +52,47 @@ const Hr = styled.hr`
 border:0.2px solid #606060;
 margin:15px 0px;
 `
+const Channel = styled.div`
+display:flex;
+justify-content:space-between;
+`
+const ChannelInfo = styled.div`
+display:flex;
+gap:15px;
+`
+const Image = styled.img`
+width:40px;
+height:40px;
+border-radius:50%;
+`
+const ChannelDetails = styled.div`
+display:flex;
+flex-direction:column;
+color:#606060;
+`
+const ChannelName = styled.span`
+font-weight:500;
+color:black;
+`
+const ChannelCounter = styled.span`
+font-size:12px;
+margin-top:5px;
+margin-bottom:10px;
+`
+const Description = styled.p`
+font-size:14px;
+color:black;
+`
+const Subscribe = styled.button`
+background-color:#cc1a00;
+font-weight:500;
+border:none;
+border-radius:3px;
+height:max-content;
+padding:7px 15px;
+cursor:pointer;
+color:#ffffff;
+`
 
 const Video = () => {
     return (
@@ -77,6 +119,18 @@ const Video = () => {
                     </Buttons>
                 </Details>
                 <Hr />
+                <Channel>
+                    <ChannelInfo>
+                        <Image src={channelImage} />
+                        <ChannelDetails>
+                            <ChannelName>Cifer Tube</ChannelName>
+                            <ChannelCounter>200k Subscriber</ChannelCounter>
+                            <Description>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio veritatis, fuga hic eos soluta deserunt?
+                            </Description>
+                        </ChannelDetails>
+                    </ChannelInfo>
+                    <Subscribe>SUBSCRIBE</Subscribe>
+                </Channel>
             </Content>
             <Recommendation>Recommendation</Recommendation>
         </Container>
